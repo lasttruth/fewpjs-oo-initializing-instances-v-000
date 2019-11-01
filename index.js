2ca0e88d344f8fe2cce315e1876948ed60cfb484
Robin Doirin <robindoirin@gmail.com>
2019-11-01 02:34:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,6 +16,9 @@ class Lunch {
 
 class Dinner {
   constructor(salad, soup, entree, dessert) {
-
+    this.salad = salad;
+    this.soup = soup;
+    this.entree = entree;
+    this.dessert = _dessert;
   }
 }
