0470247383cc93735cf9d3405000b3a283e516e5
Robin Doirin <robindoirin@gmail.com>
2019-11-01 02:28:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code here
+class Breakfast {
+  constructor(food, drink) {
+    
+  }
+}
+
+class Lunch {
+  constructor(salad, soup, drink) {
+    
+  }
+}
+
+class Dinner {
+  constructor(salad, soup, entree, dessert) {
+    
+  }
+}
